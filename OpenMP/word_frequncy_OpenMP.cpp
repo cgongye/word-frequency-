d1e938f47bef8ed0aee56e56c5cb3dcede41c2c1
george1992 <george1992@users.noreply.github.com>
2016-04-26 13:53:27 -0400

    Added files via upload

--- OpenMP/word_frequncy_OpenMP.cpp
+++ OpenMP/word_frequncy_OpenMP.cpp
@@ -4,56 +4,45 @@
 #include <ctime>        // time
 #include <string>		// string
 #include <vector>       // vector
-#include <iomanip>		// io
-#include <sstream>		// string stream
+#include <iomanip>
+#include <sstream>
 #include <omp.h>		// openmp header
 
+#define NUM 13000000
 using namespace std;
+struct letter_only: ctype<char> 
+{
+    letter_only(): ctype<char>(get_table()) {}
 
+    static ctype_base::mask const* get_table()
+    {
+        static vector<ctype_base::mask> 
+            rc(ctype<char>::table_size,ctype_base::space);
 
+        fill(&rc['A'], &rc['z'+1], ctype_base::alpha);
+        return &rc[0];
+    }
+};
 
 int main()
 {
-	clock_t start, end;
-	double cpu_time;
-	start = clock();
-	
-	map<string, int> wordCount;
 	double wall_time = omp_get_wtime();
+	map<string, int> wordCount;
 	ifstream input;
+	input.imbue(locale(locale(), new letter_only())); //enable reading only letters!
 	input.open("filename.txt");
 	string word;
-	string temp;
-	int convert;
-	// get length of file
-	input.seekg (0, input.end);
-	int length = input.tellg();
-	input.seekg (0, input.beg);
-//	printf("length is %d",length);
-	// allocate memory:
-	char * buffer = new char [length];
-	
-	// read data as a block
-	input.read (buffer, length);
-	
-	input.close();
-	word = "";
-	#pragma omp parallel for private(word, convert, temp) lastprivate(wordCount)
-	for (int i = 0; i < length; i++) {
-		convert = buffer[i];
-		temp = buffer[i];
-		if (((convert >= 65) && (convert <= 90)) || ((convert >= 97) && (convert <= 122))) {
-			word.append(temp);
-		}
-		else {
+	#pragma omp parallel for ordered shared(input, wordCount) private (word)
+	for (int i = 0; i < NUM; i++) {
+		#pragma omp ordered
+		if (input >> word) {
 			#pragma omp atomic
 			++wordCount[word];
-			word = "";
 		}
 	}
-	// free memory
-	delete[] buffer;
+	
 	double read_time = omp_get_wtime()-wall_time;
+	
 	int a = wordCount.size();
 	map<string, int>::iterator it = wordCount.begin();
 	#pragma omp parallel for ordered
@@ -63,9 +52,6 @@ int main()
 		++it;
 	}
 
- 	printf("The number of words is %d.\n",a);
 	printf("Reading time used: %f\n", read_time);
 	printf("Toltal time used: %f\n", omp_get_wtime()-wall_time);
-//	printf("%f\n", omp_get_wtime()-wall_time);
-	
 }
